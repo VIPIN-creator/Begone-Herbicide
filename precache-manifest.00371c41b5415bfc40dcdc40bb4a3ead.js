d636049f8e9d065a555dd0d86b247104d62bd94d
Vipin <vipintripathi121@gmail.com>
2020-08-09 23:21:29 +0530

    Updates

---
+++ precache-manifest.00371c41b5415bfc40dcdc40bb4a3ead.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc87a2fe1eabc859a4fd25207070c7f7",
+    "url": "/Begone-Herbicide/index.html"
+  },
+  {
+    "revision": "a424e3ec13d1363feb4c",
+    "url": "/Begone-Herbicide/static/css/2.ece8041d.chunk.css"
+  },
+  {
+    "revision": "5de4de48c8594cc7b07b",
+    "url": "/Begone-Herbicide/static/css/main.fa6c7a91.chunk.css"
+  },
+  {
+    "revision": "a424e3ec13d1363feb4c",
+    "url": "/Begone-Herbicide/static/js/2.b0bd6414.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Begone-Herbicide/static/js/2.b0bd6414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5de4de48c8594cc7b07b",
+    "url": "/Begone-Herbicide/static/js/main.222857ff.chunk.js"
+  },
+  {
+    "revision": "97e11c4ec78b5a6cb732",
+    "url": "/Begone-Herbicide/static/js/runtime-main.57e3299d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Begone-Herbicide/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Begone-Herbicide/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Begone-Herbicide/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Begone-Herbicide/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Begone-Herbicide/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Begone-Herbicide/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Begone-Herbicide/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Begone-Herbicide/static/media/revicons.2feb69cc.eot"
+  }
+]);
