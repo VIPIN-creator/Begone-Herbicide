955be7ffee272f250890be377f1ebd7fcc8e1b09
VIPIN-creator <59695863+VIPIN-creator@users.noreply.github.com>
2020-09-15 09:21:40 +0530

    add hit counter

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -43,6 +43,9 @@ function Footer(props) {
                 <div className="col-auto">
                     <p>© Copyright 2002 Begone Herbicide</p>
                 </div>
+	    <div className = "col ml-auto">
+	    <div class="powr-hit-counter" id="37fb65dd_1600140680"></div><script src="https://www.powr.io/powr.js?platform=react"></script>
+	    </div>
             </div>
             <ScrollToTop />
         </div>
